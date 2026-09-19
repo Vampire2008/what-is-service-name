ab6b8f9050426363717753339a2ccdb61f69d708
Mikhail Kislenko <mikhail.kislenko@devexpress.com>
2026-09-19 22:55:15 +0400

    Fix test

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -4,12 +4,13 @@ import { mount } from "@vue/test-utils";
 import App from "@/App.vue";
 import { RouterView } from "vue-router";
 import router from "@/router";
+import i18n from "@/i18n";
 
 describe("App", () => {
 	test("mounts renders properly", () => {
 		const wrapper = mount(App, {
 			global: {
-				plugins: [router],
+				plugins: [router, i18n],
 			},
 		});
 		const components = wrapper.findAllComponents(RouterView);
